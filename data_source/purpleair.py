c25efc33c54ef2b422b4a86a4a0bbd3e6cdad9b0
Alan Lai <alan.n.lai@gmail.com>
2020-09-30 22:48:23 -0700

    added python-aqi package

--- data_source/purpleair.py
+++ data_source/purpleair.py
@@ -4,7 +4,7 @@ https://www2.purpleair.com/community/faq#!hc-access-the-json
 """
 
 import requests
-
+import aqi
 
 def get_points(sensor_id):
     purple_query = requests.get(f"https://www.purpleair.com/json?show={sensor_id}")
@@ -22,6 +22,8 @@ def get_points(sensor_id):
                     "pm2_5": float(sensor["pm2_5_atm"]),
                     "pm1_0": float(sensor["pm1_0_atm"]),
                     "pm10_0": float(sensor["pm10_0_atm"]),
+                    # calculates the aqi from the particulate concentrations
+                    "aqi": aqi.to_aqi([(aqi.POLLUTANT_PM25, sensor["pm2_5_atm"]),(aqi.POLLUTANT_PM10, sensor["pm10_0_atm"])])
                 },
             }
         )
